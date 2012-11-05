d9dbe08870afc3e0ef2e3b8657dc73d8c3eacacc
Wes Mason <wes@1stvamp.org>
2012-11-05 23:34:23 +0000

    Add account/host checking and outgoing request to SD

--- sd_proxy/app.py
+++ sd_proxy/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import requests
+from flask import Flask, request
 from gevent.monkey import patch_all
 
 from . import settings
@@ -6,10 +7,32 @@ from . import settings
 app = Flask(__name__)
 patch_all()
 
+ALLOWED_HOSTS = []
+
+if not settings.allow_all_accounts:
+    for account in settings.allowed_accounts:
+        if not account.endswith('.serverdensity.com'):
+            account = "{0}.serverdensity.com".format(account)
+
+        ALLOWED_HOSTS.append(account)
+
 
 @app.route('/postback/', methods=('POST',))
 def postbacks():
-    return u'OK'
+    host = request.headers.get('host', '')
+    if ':' in host:
+        host = host.split(':')[0]
+
+    if settings.allow_all_accounts or host in ALLOWED_HOSTS:
+        hash = request.form.get('hash', '')
+        payload = request.form.get('payload', '')
+
+        protocol = 'https' if settings.use_outbound_ssl else 'http'
+
+        postback_url = '{0}://{1}/'.format(protocol, host)
+        requests.post(postback_url, data={'hash': hash, 'payload': payload})
+
+    return u'"OK"'
 
 if __name__ == '__main__':
     app.run()
