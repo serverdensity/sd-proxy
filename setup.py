e9226e81e23a3564035404b7bcc0a9ef1319e93d
Wes Mason <wes@1stvamp.org>
2012-11-05 15:50:55 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+"""Installer for sd-proxy
+"""
+
+import os
+cwd = os.path.dirname(__file__)
+
+try:
+        from setuptools import setup, find_packages
+except ImportError:
+        from ez_setup import use_setuptools
+        use_setuptools()
+        from setuptools import setup, find_packages
+
+setup(
+    name='sd-proxy',
+    description='Intelligent HTTP proxy for proxying Server Density'
+                ' sd-agent payloads from within a private network',
+    long_description=open('README.rst').read(),
+    version='1.0.0',
+    author='Wes Mason',
+    author_email='wes@serverdensity.com',
+    url='https://github.com/serverdensity/sd-proxy',
+    packages=find_packages(exclude=['ez_setup']),
+    install_requires=open('requirements.txt').readlines(),
+    include_package_data=True,
+    license='BSD',
+    entry_points={
+        'console_scripts': [
+            'sd-proxy = serverdensity.sd_proxy.runserver:main'
+        ]
+    }
+)
